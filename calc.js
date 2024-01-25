7936ea5274240f3e7d5ea26a974669ef91b99109
Nico <ndiaz413@west-mec.org>
2024-01-24 20:42:04 -0700

    Update calc.js and fix calc.css
    Updated functions inside calc.js and fixed the calc.css to show up in the middle of the screen

--- calc.js
+++ calc.js
@@ -1,5 +1,32 @@
-let input = document.getElementById('equal');
-function coolio(){
-    input.value = ;
+let input = document.getElementById('target');
+function clear(){
+    input.value = "";
 }
 
+function del(){
+    input.value = input.value.slice(0, -1);
+}
+
+function number(num) {
+    input.value += num;
+}
+
+function decimal() {
+    input.value += '.';
+}
+  
+function divide() {
+    input.value += '/';
+} 
+
+function multiply() {
+    input.value += '*';
+}
+  
+function minus() {
+    input.value += '-';  
+}
+  
+function plus() {
+    input.value += '+';
+}
