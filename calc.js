d0abed9671d1651f5616337c6c884cb522883449
Nico <ndiaz413@west-mec.org>
2024-02-13 18:21:21 -0700

    Update calc.js
    Updated all functions and added functionality for all buttons

--- calc.js
+++ calc.js
@@ -1,33 +1,48 @@
-function coolio(value) {
+const coolio = (value) => {
     document.getElementById("target").value += value;
 }
 
-function del() {
+const del = () => {
     let currentValue = document.getElementById("target").value;
     document.getElementById("target").value = currentValue.slice(0, -1);
 }
 
-function divide() {
+const divide = () => {
     document.getElementById("target").value += "/";
 }
 
-function multiply() {
+const multiply = () => {
     document.getElementById("target").value += "*";
 }
 
-function subtract() {
+const subtract = () => {
     document.getElementById("target").value += "-";
 }
 
-function add() {
+const add = () => {
     document.getElementById("target").value += "+";
 }
 
-function clear() {
-    
+const clearScreen = () => {
+    document.getElementById("target").value = ' ';
 }
 
-function equals() {
-    let equation = document.getElementById("target").value;
-    
+const equals = () => {
+    let expression = document.getElementById("target").value;
+    expression = expression.replace(/sin/g, 'Math.sin').replace(/cos/g, 'Math.cos').replace(/tan/g, 'Math.tan').replace(/arcsin/g, 'Math.asin').replace(/arccos/g, 'Math.acos').replace(/arctan/g, 'Math.atan').replace(/ln/g, 'Math.log').replace(/log/g, 'Math.log10').replace(/pi/g, 'Math.PI').replace(/e/g, 'Math.E');
+    let result = calculate(expression);
+    document.getElementById("target").value = result;
+}
+
+const calculate = (expression) => {
+    try {
+        const coolZone = new Function('return ' + expression);
+        return coolZone();
+    } catch(error) {
+        return 'Error';
+    }
+}
+
+const openDrawer = () => {
+    document.getElementById('trig-buttons').classList.toggle('active');
 }
